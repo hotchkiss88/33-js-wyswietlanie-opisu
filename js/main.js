540ee33d6c5f74d5635e2fbc1618e9053801d435
hotchkiss88 <al-sowa@hotmail.com>
2021-08-31 15:30:52 +0200

    33-js-wyswietlanie-opisu

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let item = document.getElementById("opis");
+let knop = document.querySelector("button");
+
+knop.onclick = function() { item.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec pharetra vestibulum libero ac auctor.Mauris vel nunc metus.Sed neque."; }
